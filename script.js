65b43c8913eeefde52c3945057a18cec7575864e
Raghav Thapar <raghavthapar.apps@gmail.com>
2023-08-24 17:43:33 +0530

    Adding the next and previous button functionality

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const rightbtn = document.querySelector(".btn-right");
 const leftbtn = document.querySelector(".btn-left");
 const panels = document.querySelectorAll(".panel");
 let activePanel = document.querySelector(".active");
+let activePanelIndex = 0;
 
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
@@ -11,4 +12,17 @@ panels.forEach((panel) => {
     panel.classList.add("active");
     activePanel = panel;
   });
+});
+
+// Keeping the starting index of 0
+rightbtn.addEventListener("click", function () {
+  panels[activePanelIndex].classList.remove("active");
+  activePanelIndex++;
+  panels[activePanelIndex].classList.add("active");
+});
+
+leftbtn.addEventListener("click", function () {
+  panels[activePanelIndex].classList.remove("active");
+  activePanelIndex--;
+  panels[activePanelIndex].classList.add("active");
 });
